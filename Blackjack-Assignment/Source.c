0f50cf428f602e5aac980c0074ad88dc366f6feb
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-12-15 00:10:40 +0000

    new stuff

--- Blackjack-Assignment/Source.c
+++ Blackjack-Assignment/Source.c
@@ -12,7 +12,7 @@ int cardDeck[52] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
 					1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 int cardNum = 51;
 int playerCards[4][10];
-int playerTrun= 0;
+int playerTrun = 0, whichPlayer = 0;
 
 // display the deck
 void DisplayDeck(int deck[52])
@@ -25,11 +25,70 @@ void DisplayDeck(int deck[52])
 
 // show player hand
 void displayPlayerHand(int player) {
-	printf("Player %d -", (player + 1));
+
+	int totalCardNum = 0, CardNum = 0, ace = 0;
+
+	// if player one is the dealer
+	if (player == 0)
+		printf("Dealer hand -");
+	else
+		printf("Player %d hand -", player);
+
 	for (int i = 0; i < playerTrun; i++) {
-		printf(" %d", playerCards[player][i]);
+		CardNum = 0;
+
+		// what card is it e.g. is it a king, queen, jack or ace
+		if (playerCards[player][i] == 1) {
+			printf(" Ace");
+		}
+		else if (playerCards[player][i] == 11) {
+			printf(" Jack"); 
+			CardNum = 10;
+		}
+		else if (playerCards[player][i] == 12) {
+			printf(" Queen"); 
+			CardNum = 10;
+		}
+		else if (playerCards[player][i] == 13) {
+			printf(" King");
+			CardNum = 10;
+		}
+		else {
+			printf(" %d", playerCards[player][i]);
+			CardNum = playerCards[player][i];
+		}
+
+		totalCardNum = totalCardNum + CardNum;
 	}
+
+	// ask user if they want ace to be 1 or 11
+	for (int i = 0; i < playerTrun; i++) {
+		CardNum = 0;
+		if (playerCards[player][i] == 1) {
+			do {
+				printf("\nDo you what the Ace to be a 1 or 11 (Enter 1 or 11)");
+				scanf_s("%d", &ace);
+				if (ace == 1)
+					CardNum = 1;
+				else if (ace == 11)
+					CardNum = 11;
+				else {
+					printf("Enter 1 or 11 for your ace");
+					ace = 0;
+				}
+			} while (ace == 0);
+		}
+
+		totalCardNum = totalCardNum + CardNum;
+	}
+
+	printf(" = %d", totalCardNum);
 	printf("\n");
+}
+
+
+void isItAce(int CardNum, int player, int ace) {
+
 }
 
 // hand out the cards to players
@@ -39,9 +98,9 @@ void fristHandCardOut(int players) {
 	{
 		for (int j = 0; j < players; j++)
 		{
-			playerCards[i][j] = cardDeck[cardNum];
+			playerCards[j][i] = cardDeck[cardNum];
 			cardNum--;
-			printf("%d - %d\n", playerCards[i][j], (j + 1));
+			printf("%d - %d\n", playerCards[j][i], (j + 1));
 		}
 		playerTrun++;
 	}
@@ -61,7 +120,7 @@ void shuffleCards() {
 		cardDeck[i] = cardDeck[drawnCard];
 		cardDeck[drawnCard] = temp;
 	}
-	DisplayDeck(cardDeck);
+	// DisplayDeck(cardDeck);
 }
 
 void main()
@@ -78,7 +137,7 @@ void main()
 	do
 	{
 		printf("How Many player are there to play (player one is the Dealer and you need 2 or 4 player to start playing)\n");
-		scanf("%d", &howManyPlayers);
+		scanf_s("%d", &howManyPlayers);
 
 		if (howManyPlayers >= 2 && howManyPlayers <= 4)
 		{
